568cf1d3135a4329acd23e5004eab279a1c83fea
JohnDehiby25 <dehibymorales@gmail.com>
2026-05-01 22:12:31 -0400

    Creacion del struct de socio, tomado del ejercicio que se habia iniciado anteriormente

---
+++ gimnasio/lib/socio.ex
@@ -0,0 +1,27 @@
+defmodule Socio do
+  @enforce_keys [:nombre, :edad]
+  defstruct [:nombre, :edad, clases: []]
+
+  def nuevo(nombre, edad) when edad > 0 and edad < 100 do
+    {:ok, %__MODULE__{nombre: nombre, edad: edad}}
+  end
+
+  def nuevo(_, _), do: {:error, :edad_invalida}
+
+  def inscribir_clase(%__MODULE__{clases: clases} = socio, clase) do
+    if clase in clases do
+      {:error, :ya_inscrito}
+    else
+      {:ok, %{socio | clases: [clase | clases]}}
+    end
+  end
+
+  def desinscribir_clase(%__MODULE__{clases: clases} = socio, clase) do
+    {:ok, %{socio | clases: List.delete(clases, clase)}}
+  end
+
+  def tiene_clase?(%__MODULE__{clases: clases}, clase) do
+    clase in clases
+  end
+
+end
