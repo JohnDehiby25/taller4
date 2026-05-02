e14336e666b0ca6299b22bb4fafc8ff43242bcdf
JohnDehiby25 <dehibymorales@gmail.com>
2026-05-01 22:16:45 -0400

    Creacion de los metodos del crud en el modulo de gimnasio.ex

--- gimnasio/lib/gimnasio.ex
+++ gimnasio/lib/gimnasio.ex
@@ -1,18 +1,42 @@
 defmodule Gimnasio do
-  @moduledoc """
-  Documentation for `Gimnasio`.
-  """
+  def agregar_socio(socios, cedula, nombre, edad) do
+    case Socio.nuevo(nombre, edad) do
+      {:ok, socio} ->
+        if Map.has_key?(socios, cedula) do
+          {:error, :cedula_duplicada}
+        else
+          nuevos = Map.put(socios, cedula, socio)
+          guardar_datos(nuevos)
+          {:ok, nuevos}
+        end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Gimnasio.hello()
-      :world
-
-  """
-  def hello do
-    :world
+      error -> error
+    end
   end
+
+  def actualizar_socio(socios, cedula, nombre, edad) do
+    case Map.get(socios, cedula) do
+      nil ->
+        {:error, :no_encontrado}
+
+      socio ->
+        actualizado = %{socio | nombre: nombre, edad: edad}
+        nuevos = Map.put(socios, cedula, actualizado)
+        guardar_datos(nuevos)
+        {:ok, nuevos}
+    end
+  end
+
+  def eliminar_socio(socios, cedula) do
+    if Map.has_key?(socios, cedula) do
+      nuevos = Map.delete(socios, cedula)
+      guardar_datos(nuevos)
+      {:ok, nuevos}
+    else
+      {:error, :no_encontrado}
+    end
+  end
+
+
+
 end
